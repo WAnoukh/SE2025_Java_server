1b4d351fc604887adf2241b221518911aa0e177f
AnoukhW <anoukhman@gmail.com>
2025-01-19 09:34:53 +0100

    Added updateVolunteer command

--- src/main/java/Javabackend/GovernmentVolunteer/Volunteer/VolunteerController.java
+++ src/main/java/Javabackend/GovernmentVolunteer/Volunteer/VolunteerController.java
@@ -107,52 +107,8 @@ public class VolunteerController {
         addVolunteer(volunteer);
     }
 
-    public static void main(String[] args) throws Exception {
-        Volunteer volunteer = new Volunteer(
-                "Wach", "Anouk",
-                true, "Rue de la Tour 1",
-                "1000", "Lausanne", "Switzerland",
-                "678c9729856283f299c918cf"
-                );
-
-        VolunteerController vController = VolunteerController.getInstance();
-        vController.updateVolunteer(volunteer);
-
-        /*if (args.length > 0) {
-            if (args[0].equals("test")) {
-                // load test parameters
-            } else if (args[0].equals("production")) {
-                // load production parameters
-            }
-        }
-        VolunteerController vController = new VolunteerController();
-        List<Volunteer> volunteers = vController.getVolunteers();
-        for (Volunteer volunteer : volunteers) {
-            System.out.println(volunteer.getLastName());
-        }*/
-
-
-        /*URL yahoo = new URL("https://hacs.bdemir.net/auth/signup");
-        URLConnection yc = yahoo.openConnection();
-        HttpURLConnection con = (HttpURLConnection)yahoo.openConnection();
-        con.setRequestMethod("GET");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("Accept", "application/json");
-        con.setDoOutput(true);
-        String jsonInputString = "{\"name\":\"Anoukh\" ,\"email\" : \"anoukhan.wachnicki@gmail.com\", \"password\": \"123\", \"role\":\"Volunteer\"}";
-        try(OutputStream os = con.getOutputStream()) {
-            byte[] input = jsonInputString.getBytes("utf-8");
-            os.write(input, 0, input.length);
-        }
-        try(BufferedReader br = new BufferedReader(
-                new InputStreamReader(con.getInputStream(), "utf-8"))) {
-            StringBuilder response = new StringBuilder();
-            String responseLine = null;
-            while ((responseLine = br.readLine()) != null) {
-                response.append(responseLine.trim());
-            }
-            System.out.println(response.toString());
-        }
-        System.out.println(con.getResponseMessage());*/
+    public void updateVolunteer(String lastName, String firstName, boolean validated, String street, String postalCode, String city, String country, String userId) throws JavaBackendException {
+        Volunteer volunteer = new Volunteer(lastName, firstName, validated, street, postalCode, city, country, userId);
+        updateVolunteer(volunteer);
     }
 }
